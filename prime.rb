952cba77faba0c10e960ba29fd3ec2f369b9bb03
Chloe Gell <gellert.chloe@gmail.com>
2020-01-24 21:11:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,13 @@ def prime?(num)
   return false if num.even? && num != 2 
   range = []
   num/2 = upper 
-  for(i = 1; i < upper; i++)
+  for(i = 1; i <= upper; i++)
     range << i 
   end
-  
+  range.each do |j|
+    if(num % j == 0)
+      return false 
+    end 
+  end
+  return true 
 end 
